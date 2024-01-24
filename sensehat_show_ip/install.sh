c1a9fcbc3f4c3f8733cbc708b25936ee9d7810ea
Trond F. Christiansen <trochr@hotmail.com>
2024-01-24 17:21:33 +0100

    fixed install path

--- sensehat_show_ip/install.sh
+++ sensehat_show_ip/install.sh
@@ -1,6 +1,6 @@
 #!bin/bash
-cp sensehat_show_ip.py /sensehat_show_ip.py # copy the python script to the root directory
-cp sensehat_show_ip.service /etc/systemd/system/sensehat_show_ip.service # copy the service file to the systemd service directory
+cp ~/RPI-utilities/sensehat_show_ip/showip.py /showip.py # copy the python script to the root directory
+cp ~/RPI-utilities/sensehat_show_ip/showip.service /etc/systemd/system/sensehat_show_ip.service # copy the service file to the systemd service directory
 sudo systemctl daemon-reload # reload the systemd daemon
 sudo systemctl enable sensehat_show_ip.service # enable the service
 sudo systemctl start sensehat_show_ip.service # start the service
